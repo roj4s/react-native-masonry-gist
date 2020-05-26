63290b189d80fe5cb3c156021e85d16f6c87fabe
Luis Miguel Rojas Aguilera <rojas@icomp.ufam.edu.br>
2020-05-26 13:07:55 -0400

    Arrange images using flexWrap
    Signed-off-by: Luis Miguel Rojas Aguilera <rojas@icomp.ufam.edu.br>

--- components/Masonry.js
+++ components/Masonry.js
@@ -1,13 +1,17 @@
 import React from 'react';
 import {
-  View, Image, StyleSheet, ScrollView
+  View, Image, StyleSheet, ScrollView, Dimensions
 } from 'react-native';
 
 
 export default function Masonry({data}) {
   return (
     <ScrollView style={styles.container}>
-        <View>
+        <View 
+            style={{
+                    height: vpHeight * data.length / 3,
+                    flexWrap: 'wrap'
+                }}>
             {
                 data.map((item)=>
                 <View 
@@ -25,15 +29,16 @@ export default function Masonry({data}) {
  );
 }
 
+const vpWidth = Dimensions.get('window').width;
+const vpHeight = Dimensions.get('window').height;
+
 const styles = StyleSheet.create({    
     view: {
-        margin: 8,
-        alignContent: 'center',
-        alignItems: 'center'        
+        margin: 8             
     },
     img: {
-        width: 300,
-        height: 300,
+        width: vpWidth / 2 - 15,
+        height: vpWidth,
         borderRadius: 5
     }
 });
